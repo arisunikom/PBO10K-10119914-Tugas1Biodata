18f39e62ff66b5356b2f82f39175ca087deedc16
dropsuite <dropsuite@172.20.10.2>
2020-10-20 19:40:23 +0700

    Test commit

---
+++ src/main/java/Tugas1Biodata/Main.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tugas1Biodata;
+
+/**
+ *
+ * @author Aris Prabowo
+ * IF 10K
+ * 10119914
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "---------------------");
+        System.out.println("NAMA\t\t: Aris Prabowo");
+        System.out.println("Alamat\t\t: Bandung Soekarno Hatta");
+        System.out.println("TEMPAT LAHIR\t: Cirebon");
+        System.out.println("TANGGAL LAHIR\t: 8 Maret 1997");
+        System.out.println("---------------------");
+        System.out.println("HOBI\t\t: Nonton Teori OnePiece");
+        System.out.println("MAKES\t\t: Kentang Goreng");
+        System.out.println("MIKES\t\t: Cola");
+    }
+    
+}
